0562d099cfc90019d0b097ce1b63c17a9853b5f6
Alexis Parave <aparavev@gmail.com>
2022-08-20 17:31:56 -0500

    FEAT: add and run tests for services

--- spec/request_helper.rb
+++ spec/request_helper.rb
@@ -1,7 +1,7 @@
-require 'vcr'
+require "vcr"
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/cassettes'
+  config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
 end
