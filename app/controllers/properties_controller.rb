6eaec4ed1f1e2225f62849a7aa2350f4d8e23509
Alexis Parave <aparavev@gmail.com>
2022-08-19 23:30:18 -0500

    FEAT: add functionality to connect with EasyBroker API and list properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,17 @@
 class PropertiesController < ApplicationController
-  def index; end
+  before_action :initial_config
+
+  def index
+    res = HTTParty.get("#{@base_url}/properties?page=1&limit=15&search%5Bstatuses%5D%5B%5D=published", @options)
+    @properties = res["content"]
+  end
+
+  private
+
+  def initial_config
+    @base_url = "https://api.stagingeb.com/v1"
+    @options = {
+      headers: { "X-Authorization": "l7u502p8v46ba3ppgvj5y2aad50lb9" }
+    }
+  end
 end
